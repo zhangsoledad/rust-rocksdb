a309d1677b330e7069d17ae54715c3768d487c67
zhangsoledad <787953403@qq.com>
2021-01-06 23:38:06 +0800

    chore: bump rocksdb up to 6.11.6

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0915c99f01b46f50af8e02da8b6528156f584b7c@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/03/18 18:51:16@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@878b3a41de96aefd67e9d94860b60a1bedf6320f@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/10/12 12:44:33@";
 const char* rocksdb_build_compile_date = __DATE__;
